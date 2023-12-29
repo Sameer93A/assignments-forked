24e211e047c7cc49040d8787b6a0158de9537094
Sameer Ahmed <sameer937655@gmail.com>
2023-12-29 13:09:05 +0530

    after saving

--- week-1/offline-class-1/level-1/01-String.js
+++ week-1/offline-class-1/level-1/01-String.js
@@ -8,69 +8,69 @@
 // Length
 function getLength(str) {
   console.log("Original String:", str);
-  console.log("Length:", str.length);
+  console.log("Length:", str.length); // Length: 11
 }
 getLength("Hello World");
 
 // indexOf
 function findIndexOf(str, target) {
   console.log("Original String:", str);
-  console.log("Index:", str.indexOf(target));
+  console.log("Index:", str.indexOf(target)); // Index: 6
 }
 findIndexOf("Hello World", "World");
 
 // lastIndexOf
 function findLastIndexOf(str, target) {
   console.log("Original String:", str);
-  console.log("Index:", str.lastIndexOf(target));
+  console.log("Index:", str.lastIndexOf(target)); // Index: 12
 }
 findLastIndexOf("Hello World World", "World");
 
 // slice
 function getSlice(str, start, end) {
   console.log("Original String:", str);
-  console.log("After slice:", str.slice(start, end));
+  console.log("After slice:", str.slice(start, end)); // After slice: Hello
 }
 getSlice("Hello World", 0, 5);
 
 // substring
 function getSubstring(str, start, end) {
   console.log("Original String:", str);
-  console.log("After substring:", str.substring(start, end));
+  console.log("After substring:", str.substring(start, end)); // After substring: Hello
 }
 getSubstring("Hello World", 0, 5);
 
 // replace
 function replaceString(str, target, replacement) {
   console.log("Original String:", str);
-  console.log("After replace:", str.replace(target, replacement));
+  console.log("After replace:", str.replace(target, replacement)); // After replace: Hello JavaScript
 }
 replaceString("Hello World", "World", "JavaScript");
 
 // split
 function splitString(str, separator) {
   console.log("Original String:", str);
-  console.log("After split:", str.split(separator));
+  console.log("After split:", str.split(separator)); // After split: [ 'Hello', 'World' ]
 }
 splitString("Hello World", " ");
 
 // trim
 function trimString(str) {
   console.log("Original String:", str);
-  console.log("After trim:", str.trim());
+  console.log("After trim:", str.trim()); // After trim: Hello World
 }
 trimString(" Hello World ");
 
 // toUpperCase
 function toUpper(str) {
   console.log("Original String:", str);
-  console.log("After toUpperCase:", str.toUpperCase());
+  console.log("After toUpperCase:", str.toUpperCase()); // After toUpperCase: HELLO WORLD
 }
 toUpper("Hello World");
 
 // toLowerCase
 function toLower(str) {
   console.log("Original String:", str);
-  console.log("After toLowerCase:", str.toLowerCase());
+  console.log("After toLowerCase:", str.toLowerCase()); // After toLowerCase: hello world
 }
 toLower("Hello World");
